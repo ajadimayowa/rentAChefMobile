c0de1e76836dd4622494a1b56fcce3fbf7ae57d1
ajadimayowa <samtalktech@outlook.com>
2026-03-22 01:56:32 +0100

    updated version

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default {
     name: 'rentAChef',
     slug: 'rentAChef',
     scheme: 'rentAChef',
-    version: '2.0.0',
+    version: '3.0.0',
     orientation: 'portrait',
     icon: './assets/loadIcon.png',
     userInterfaceStyle: 'automatic',
